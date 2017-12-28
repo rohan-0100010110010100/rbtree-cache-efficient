7a932fd279e7c98961f85b8108ea7bf521a7df31
Rohan Verma <rohan40594@gmail.com>
2017-12-29 04:31:38 +0530

    Update test_program.cpp

--- test_program.cpp
+++ test_program.cpp
@@ -1,19 +1,12 @@
-#include <iostream>
 #include <RedBlack.h>
 
 Redblack<int> t_tree(0);
-
-
-
 
 int main(){
 
   for(int x = 1 ; x<100000;++x)
     t_tree.insert(x);
 
-
 t_tree.printTree(t_tree.base);
-
-
 
 }
